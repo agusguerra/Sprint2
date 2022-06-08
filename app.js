350526909fedbfd4eb13bb26247ac824288564bf
Agustín <105322348+agstino@users.noreply.github.com>
2022-06-08 19:17:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 //cuando el usuario presiona el botón "buttonClick" se crea el constructor persona,
 //obteniendo su nombre y lo que gastó, y llevándo esos datos a un objeto. Posteriormente
 //se ejecuta una función "addExpense"
+
+var acu = 0;
+var promedio = 0;
+
 function buttonClick(){
     //constructor
     function persona(nombre, gasto){
@@ -9,23 +13,27 @@ function buttonClick(){
     }
     var get_nombre = document.getElementById("nombre").value;
     var get_gasto = document.getElementById("gasto").value;
+    
+    acu += get_gasto;
+    console.log(acu);
     //variable global (sin var)
     nueva_persona = new persona(get_nombre, get_gasto);
-    addExpense();
 
-   /* let total = 0, prom = 0;
-    total += get_gasto;
-    let prom=0;
-    prom = total/ */
+    addExpense();
 }
+
 
 var array_datos = [];
 
 function addExpense(){
-    array_datos.push(nueva_persona);
-    document.getElementById("tabla").innerHTML += '  <tbody><td>'+nueva_persona.nombre+'</td><td>'+nueva_persona.gasto+'</td></tbody>';
 
+    array_datos.push(nueva_persona);
+    document.getElementById("tabla").innerHTML += '<tbody><td>'+nueva_persona.nombre+'</td><td>'+nueva_persona.gasto+'</td></tbody>';
 }
 
+var result_gastos = document.getElementById("result_gastos");
+console.log(acu);
+result_gastos.innerHTML='Total: $'+acu; 
 
-
+var prom = document.getElementById("prom");
+prom.innerHTML='Debe pagar c/u: $'+promedio;
